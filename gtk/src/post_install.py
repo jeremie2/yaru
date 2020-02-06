42d6ace7028e79af8bc8afb2197b3e31f111c302
Carlo Lobrano <c.lobrano@gmail.com>
2020-02-06 17:43:45 +0100

    flavours: refactoring build scripts
    - remove meson.build duplication

---
+++ gtk/src/post_install.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vi: set ft=python :
+
+import sys
+from os import environ, path
+from shutil import move
+
+PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr')
+data_dir = sys.argv[1]
+project_name = sys.argv[2]
+flavours = sys.argv[3:]
+
+themes_dir = path.join(PREFIX, data_dir, 'themes')
+
+for f in flavours:
+    flavour_name = "{project}-{flavour}".format(project=project_name, flavour=f)
+
+    # rename index.theme
+    theme_index_name = "{project}-{flavour}-index.theme".format(project=project_name, flavour=f)
+    theme_index_src = path.join(themes_dir, flavour_name, theme_index_name)
+
+    print('searching ', theme_index_src)
+    if path.exists(theme_index_src):
+        theme_index_dst = path.join(themes_dir, flavour_name, 'index.theme')
+
+        print('Replace %s with %s' % (theme_index_src, theme_index_dst))
+        move(theme_index_src, theme_index_dst)
+
+
+    # rename gresource, gtk.css and gtk-dark.css
+    for gtkver in ['3.20', '3.0']:
+        theme_gresource = "{project}-{flavour}-gtk-{ver}.gresource".format(project=project_name, flavour=f, ver=gtkver)
+        theme_gresource_src = path.join(themes_dir, flavour_name, 'gtk-' + gtkver, theme_gresource)
+
+        print('searching ', theme_gresource_src)
+        if path.exists(theme_gresource_src):
+            theme_gresource_dst = path.join(themes_dir, flavour_name, 'gtk-' + gtkver, 'gtk.gresource')
+            print('Replace %s with %s' % (theme_gresource_src, theme_gresource_dst))
+            move(theme_gresource_src, theme_gresource_dst)
+
+        for variant in ['', '-dark']:
+            theme_gtk_css = "{project}-{flavour}-gtk-{ver}{variant}.css".format(project=project_name, flavour=f, ver=gtkver, variant=variant)
+            theme_gtk_css_src = path.join(themes_dir, flavour_name, 'gtk-' + gtkver, theme_gtk_css)
+
+            print('searching ', theme_gtk_css_src)
+            if path.exists(theme_gtk_css_src):
+                theme_gtk_css_dst = path.join(themes_dir, flavour_name, 'gtk-' + gtkver, 'gtk{variant}.css'.format(variant=variant))
+                print('Replace %s with %s' % (theme_gtk_css_src, theme_gtk_css_dst))
+                move(theme_gtk_css_src, theme_gtk_css_dst)
+
+
+
